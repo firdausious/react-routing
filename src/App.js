2b022d726b76e2dce5a0c801b96c93cc24a845f1
Firdaus <firdausious@gmail.com>
2020-10-20 20:57:30 +0700

    init with react-routes v6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { BrowserRouter, Navigate, Routes, Route, Link, Outlet, useParams } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/skills" element={<Skills isAuthenticated={false} />} />
+            <Route path="/about">
+              <Route path="/" element={<About />} />
+              <Route path=":id" element={<AboutDetail />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
 }
+
+const Home = () => (
+  <div>
+    <Menu />
+
+    <p>Ini Home</p>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <Menu />
+
+    <p>Ini About</p>
+
+    <Outlet />
+
+    <ul>
+      <li><Link to="ari">Ari</Link></li>
+      <li><Link to="nate">Nate</Link></li>
+    </ul>
+  </div>
+)
+
+const AboutDetail = () => {
+  let { id } = useParams()
+
+  return (
+    <div>
+      <Menu />
+
+      <p>Ini Detail about -> {id} </p>
+
+      <Link to="/about">Back</Link>
+    </div>
+  )
+}
+
+const Skills = ({ isAuthenticated }) => (
+  <div>
+    <Menu />
+
+    {isAuthenticated && <Navigate to="/" />}
+
+    <p>Ini Skills, harusnya tampil nih saat akses /skill, cuman gara-gara component Navigate di atas, otomatis di redirect ke home</p>
+  </div>
+)
+
+const Menu = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/skills">Skills</Link>
+    <Link to="/about">About</Link>
+  </nav>
+)
 
 export default App;
